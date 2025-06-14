26804156c2ae631652061315d60066a428cf2732
Jonathas <152817664+Jonatha0942S@users.noreply.github.com>
2025-06-14 02:12:27 +0000

    Prova Concluida

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,24 +15,33 @@ export function Main() {
             <img src={Moinho} alt="Random" className={styles.cardImage} />
             <div className={styles.cardContent}>
               <h2>Moinho Holandês</h2>
-              <p> O Moinho Holandês, situado em Macau-RN, é um dos pontos turísticos mais representativos da cidade. Inspirado na arquitetura europeia, ele simboliza a herança cultural e a forte ligação da região com a indústria salineira. Com sua estrutura imponente, chama a atenção na paisagem urbana e atrai visitantes em busca de lazer e cultura. O espaço ao seu redor é utilizado para eventos, encontros e atividades ao ar livre, sendo também um local de convivência para a população. Além de embelezar a cidade, o moinho contribui para fortalecer o turismo e a identidade cultural de Macau.</p>
+              <p> O Moinho Holandês, situado em Macau-RN, é um dos pontos turísticos mais representativos da cidade. Inspirado na 
+                arquitetura europeia, ele simboliza a herança cultural e a forte ligação da região com a indústria salineira. 
+                Chama a atenção na paisagem urbana e atrai visitantes em busca de lazer e cultura. O espaço ao seu redor é utilizado 
+                encontros, atividades ao ar livre e é palco de muitas fotos. Além de embelezar a cidade, o moinho contribui para 
+                fortalecer o turismo e a identidade cultural de Macau.</p>
             </div>
           </div>
           <div className={styles.card}>
             <img src={Matriz} alt="Random" className={styles.cardImage} />
             <div className={styles.cardContent}>
               <h2>Igreja Matriz</h2>
-              <p>A Igreja Matriz de Nossa Senhora da Conceição, no centro de Macau-RN, é um dos principais símbolos religiosos e históricos da cidade. Construída no século XIX, destaca-se por sua torre com sino e fachada clara. É dedicada à padroeira do município, cuja festa em dezembro atrai grande participação popular. Além de espaço de fé, a igreja representa a memória e identidade cultural dos macaenses, sendo também ponto de encontro da comunidade e palco de celebrações tradicionais.</p>
+              <p>A Igreja Matriz de Nossa Senhora da Conceição, no centro de Macau-RN, é um dos principais símbolos religiosos e 
+                históricos da cidade. Construída no século XIX, destaca-se por sua torre com sino e fachada clara. É dedicada à 
+                padroeira do município, cuja festa em dezembro atrai grande participação popular. Além de espaço de fé, a igreja 
+                representa a memória e identidade cultural dos macaenses, sendo também ponto de encontro da comunidade e palco de 
+                celebrações tradicionais.</p>
             </div>
           </div>
           <div className={styles.card}>
             <img src={Orla} alt="Random" className={styles.cardImage} />
             <div className={styles.cardContent}>
               <h2>Orla de Camapum</h2>
-              <p> A Praia de Camapum, localizada em Macau-RN, é um dos principais cartões-postais da cidade.
-                A orla conta com estrutura de lazer, incluindo calçadão, quiosques, duchas, iluminação e espaço para eventos. 
-                É palco de festas tradicionais, como o réveillon e o carnaval, sendo um importante ponto de convivência e lazer da população. 
-                Além de valorizar o turismo local, a praia movimenta o comércio e contribui para a geração de renda e fortalecimento da identidade cultural de Macau.</p>
+              <p> A Praia de Camapum, localizada em Macau-RN, é um dos principais cartões-postais da cidade. A orla conta com 
+                estrutura de lazer, incluindo calçadão, quiosques, duchas, iluminação e espaço para eventos. É palco de festas 
+                tradicionais, como o réveillon e o carnaval, sendo um importante ponto de convivência e lazer da população. Além 
+                de valorizar o turismo local, a praia movimenta o comércio e contribui para a geração de renda e fortalecimento da 
+                identidade cultural de Macau.</p>
             </div>
           </div>
           <div className={styles.card}>
@@ -50,9 +59,10 @@ export function Main() {
             <div className={styles.cardContent}>
               <h2>Salinor – Salinas do Nordeste S.A</h2>
               <p> A Salinor – Salinas do Nordeste S.A, é a maior produtora de sal marinho do Brasil, responsável por mais de 40% da 
-                produção nacional. Com salinas localizadas em Macau e Mossoró, a empresa produz sal para consumo humano, industrial e animal, exporta para diversos países e 
-                atende milhares de clientes. Além disso, investe em inovação e sustentabilidade, com novas embarcações para transporte 
-                e equipamentos modernos em seu parque fabril, sendo fundamental para a economia local e nacional.</p>
+                produção nacional. Com salinas localizadas em Macau e Mossoró, a empresa produz sal para consumo humano, industrial e 
+                animal, exporta para diversos países e atende milhares de clientes. Além disso, investe em inovação e sustentabilidade, 
+                com novas embarcações para transporte e equipamentos modernos em seu parque fabril, sendo fundamental para a economia 
+                local e nacional.</p>
             </div>
           </div>
         </div>
