21832420c0db236281a662a1f7dff472894b618d
Jonathas <152817664+Jonatha0942S@users.noreply.github.com>
2025-08-15 15:21:27 +0000

    Desaio Final - 2º Bimestre Concluido

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,25 @@ import { Header } from "./components/Header";
 import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
 import { CartProvider } from "./service/CartContext";
+import Login  from "./components/Login";
+import Cadastro from "./components/Cadastro";
+import GerenciarProdutos from "./components/GerenciarProdutos";
 
 export default function App() {
 
   return (
     <>
-    <CartProvider>
+    <Login />
+    <Cadastro />
+    <GerenciarProdutos />
+    {/* <CartProvider>
       <Header />
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/produtos" element={<TelaProdutos />} />
       </Routes>
-    </CartProvider>
+    </CartProvider> */}
     </>
   );
 }
-
-// import "./styles/theme.css";
-// import "./styles/global.css";
-// import { ProductList } from "./components/ProductList";
-// import { Header } from "./components/Header";
-// import { useState } from "react";
-// import { Route, Routes } from "react-router";
-// import { Cart } from "./components/Cart";
-
-// export default function App() {
-  
-//   const [cart, setCart] = useState([]);
-
-//   function addToCart(product) {
-//     setCart((prevCart) => [...prevCart, product]);
-//   }
-
-//   return (
-//     //React Fragment
-//     <>
-//       <Header cart={cart} />
-//       <Routes>
-//         <Route path="/" element={<ProductList addToCart={addToCart} />} />
-//         <Route path="/cart" element={
-//           <Cart cart={cart} setCart={setCart} clearCart={clearCart} />
-//         } />
-//       </Routes>
-//     </>
-//   );
-// }
