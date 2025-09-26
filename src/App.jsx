660d707905d0f4f3b75f5ea02b9a66ab3ddd4e62
Jonathas <152817664+Jonatha0942S@users.noreply.github.com>
2025-09-26 11:59:00 +0000

    Revert "Theme Toogle, Supabase Connect, Env Config"

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,19 @@ import { ProductList } from "./components/ProductList";
 import { Header } from "./components/Header";
 import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
-import { CartProvider } from "./service/CartContext";
-import Login  from "./components/Login";
-import Cadastro from "./components/Cadastro";
-import GerenciarProdutos from "./components/GerenciarProdutos";
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
 
   return (
     <>
-    <Login />
-    <Cadastro />
-    <GerenciarProdutos />
-    {/* <CartProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/produtos" element={<TelaProdutos />} />
-      </Routes>
-    </CartProvider> */}
+      <CartProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
