142709630a1d8c5185285ce3caed1e06cc1819ab
Jonathas <152817664+Jonatha0942S@users.noreply.github.com>
2025-11-24 23:03:31 +0000

    Persistência de carrinho usando Supabase

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/supabase': {
+        target: 'https://pkhoeevqowvttnwhjbke.supabase.co',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/supabase/, ''), 
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
